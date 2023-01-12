394efa39e07a8cf5660feae28f0fcaaeb23509b6
KHS <ewoking777@gmail.com>
2023-01-12 19:56:45 +0900

    8. JavaScript 문법 - Template Literal

---
+++ syntax/template.js
@@ -0,0 +1,9 @@
+var name = 'k8805';
+var letter = 'Dear '+name+'\n\nConsectetur ut minim consectetur esse occaecat amet sint esse ut. Adipisicing labore sint do laborum Lorem id aute. '+name+'Duis qui laboris consectetur esse do officia officia est consequat. Labore dolor ea pariatur ullamco est nulla labore tempor laborum cupidatat anim consequat. Occaecat cupidatat deserunt proident id quis esse minim ea cillum. Excepteur nulla est anim minim velit Lorem minim amet laborum. Sunt ad ad nisi id nisi ipsum veniam.'+name;
+
+var letter = `Dear ${name}
+
+Consectetur ut minim consectetur esse occaecat amet sint esse ut. ${1+1} Adipisicing labore sint do laborum Lorem id aute. ${name} Duis qui laboris consectetur esse do officia officia est consequat. Labore dolor ea pariatur ullamco est nulla labore tempor laborum cupidatat anim consequat. Occaecat cupidatat deserunt proident id quis esse minim ea cillum. Excepteur nulla est anim minim velit Lorem minim amet laborum. Sunt ad ad nisi id nisi ipsum veniam.
+${name}`;
+
+console.log(letter);
