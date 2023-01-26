1f5d4ec167461ab4595ecb723c1084668770161d
KHS <ewoking777@gmail.com>
2023-01-26 22:18:51 +0900

    25.3. JavaScript-함수의 출력

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,6 +2,8 @@ console.log(Math.round(1.6)); //2
 console.log(Math.round(1.4)); //1
 
 function sum(first, second){ // parameter 매개변수
-    console.log(first+second);
+    return first+second; // return을 만나면 함수는 실행이 종료됨
+    //console.log('b'); 실행안됨
 }
-sum(2,4); // argument 인자
+
+console.log(sum(2,4)); // argument 인자
