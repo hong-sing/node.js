8ef25179d79a7004142546170a2e46ac7a6e6031
KHS <ewoking777@gmail.com>
2023-01-26 21:17:43 +0900

    21. JavaScript-배열

---
+++ syntax/array.js
@@ -0,0 +1,12 @@
+var arr = ['A', 'B', 'C', 'D'];
+console.log(arr[1]); // B
+console.log(arr[3]); // D
+
+// C를 3으로 변경
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length); // 배열 내 요소의 갯수
+
+// 배열에 요소 추가
+arr.push('E');
+console.log(arr);
