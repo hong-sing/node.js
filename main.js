b9d733b3e79350020448cc6eaeb7e1dc3581c1ab
KHS <ewoking777@gmail.com>
2023-02-02 07:34:27 +0900

    38. App 제작 글 삭제 기능 완성

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ var app = http.createServer(function(request,response){
             var template = templateHTML(title, list, 
               `<h2>${title}</h2>${description}`, 
               `<a href="/create">create</a> <a href="/update?id=${title}">update</a> 
-              <form action="delete_porcess" method="post">
+              <form action="delete_process" method="post">
                 <input type="hidden" name="id" value="${title}">
                 <input type="submit" value="delete">
               </form>`);
@@ -137,6 +137,19 @@ var app = http.createServer(function(request,response){
             response.writeHead(302, {location: `/?id=${title}`});
             response.end();
           });
+        });
+      });
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(error){
+          response.writeHead(302, {location: `/`});
+          response.end();
         });
       });
     } else {
